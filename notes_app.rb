370a2252fa0c80c77e7d18e26a3498846fbcf1f9
Harry <harryjp1995@gmail.com>
2018-08-09 09:02:07 +0100

    Test updates.

--- notes_app.rb
+++ notes_app.rb
@@ -10,5 +10,13 @@ class Notebook
     note[title] = body
     @titles << note
   end
+
+  def search(title)
+    @titles.each { |k, v| k == title ? @titles[k, v] : nil }
+  end
 end
+
+nb = Notebook.new
+nb.add_note('A', 'abc')
+p nb.search('A')  
 
--- notes_app_spec.rb
+++ notes_app_spec.rb
@@ -9,6 +9,8 @@ describe Notebook do
 
   it { is_expected.to respond_to(:add_note).with(2).arguments }
 
+  it { is_expected.to respond_to(:search).with(1).argument }
+
   it 'Adds a note to titles when #add_note is called' do
     expect(subject.add_note('A', 'abc')).to eq [{'A' => 'abc'}]
   end
@@ -16,5 +18,10 @@ describe Notebook do
   it 'Returns all titles when #titles is called ' do
     allow(subject).to receive(:titles).and_return(%w[A B C D])
     expect(subject.titles).to eq %w[A B C D]
+  end
+
+  it 'Returns a note when #search is called' do
+    subject.add_note('A', 'abc')
+    expect(subject.search('A') == [{'A' => 'abc'}]).to eq true
   end
 end
